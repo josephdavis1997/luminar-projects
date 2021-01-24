4e42b8b138017e26ab73df1aaec64065e32df66d
josephdavis1997 <72501719+josephdavis1997@users.noreply.github.com>
2021-01-24 17:53:46 +0530

    Add files via upload

---
+++ 01_OCR_simple_reading.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 21:15:26 2020
+
+@author: chris
+"""
+#download tesseract - https://github.com/UB-Mannheim/tesseract/wiki - for windows
+#https://tesseract-ocr.github.io/tessdoc/Installation.html -for linux
+#install pytesseract - pip install pytesseract
+
+import time
+
+import cv2
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+img = cv2.imread('data/novel.png') #reading image
+start = time.time()
+print("Text extracted using Tesseract\n")
+print(pytesseract.image_to_string(img))
+end = time.time()
+t = end-start
+print("\nTime Taken   ", t, " (s)")
